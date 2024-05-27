44a7bb144043a74014d0688f80d65dc919d4d2eb
Palash Jamaiwar <palash.vdoit@gmail.com>
2024-05-27 12:56:30 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
 const app = require('express')();
+const cors = require('cors');
 var http = require('http').Server(app);
 
 const paymentRoute = require('./routes/paymentRoute');
-
+app.use(cors('*'));
 app.use('/',paymentRoute);
 
 http.listen(3000, function(){
